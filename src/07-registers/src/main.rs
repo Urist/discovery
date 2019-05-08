d1278d6be7c231eec08a15dfa4ad10b19725ddfd
Joe Barcham <jobarcha@microsoft.com>
2019-05-07 22:32:59 -0700

    07-registers with itm prints

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,29 +1,53 @@
 #![no_main]
 #![no_std]
 
+use core::ptr;
+
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
-use core::ptr;
-
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let mut itm = aux7::init().0;
 
     unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
+        const GPIOE_BSRR: u32 = 0x4800_1018;
+        const GPIOE_ODR: u32 = 0x4800_1014;
 
-        // Turn on the "North" LED (red)
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+
+        // Turn on the NORTH LED (red)
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
 
-        // Turn on the "East" LED (green)
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+
+        // Turn on the EAST LED (green)
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
 
-        // Turn off the "North" LED
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+
+        // Turn off the NORTH LED
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
 
-        // Turn off the "East" LED
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read_volatile(GPIOE_ODR as *const u16)
+        );
+
+        // Turn off the EAST LED
         ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
     }
 
