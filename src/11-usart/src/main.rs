97d5e7b6acfbed94ee14221f54738df3c040f318
Joe Barcham <jobarcha@microsoft.com>
2019-08-20 15:54:28 -0700

    11-usart - Write string with busy wait

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -9,8 +9,17 @@ use aux11::{entry, iprint, iprintln};
 fn main() -> ! {
     let (usart1, mono_timer, itm) = aux11::init();
 
-    // Send a single character
-    usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    let data = b"The quick brown fox jumps over the lazy dog.";
+
+    for character in data.iter() {
+
+        // wait until it's safe to write to TDR
+        while usart1.isr.read().txe().bit_is_clear() {}
+
+        // Send a single character
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(*character)));
+
+    }
 
     loop {}
 }
