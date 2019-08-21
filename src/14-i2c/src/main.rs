4ac073732498122ed5c245831861a2cea28eb366
Joe Barcham <jobarcha@microsoft.com>
2019-08-20 19:01:58 -0700

    14-i2c - Read single register from I2C bus

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -19,23 +19,44 @@ fn main() -> ! {
     // Stage 1: Send the address of the register we want to read to the
     // magnetometer
     {
-        // TODO Broadcast START
+        // Broadcast START
+        i2c1.cr2.write(|w| {
+            w.nbytes().bits(1); // write 1 byte
+            w.autoend().clear_bit(); // don't end since we want to read afterwards
+            w.sadd1().bits(MAGNETOMETER); // slave address
+            w.rd_wrn().clear_bit(); // WRITE mode
+            w.start().set_bit() // send START
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Write
+        // Wait for signal that we can send
+        while i2c1.isr.read().txis().bit_is_clear() {}
 
-        // TODO Send the address of the register that we want to read: IRA_REG_M
+        // Send the address of the register that we want to read: IRA_REG_M
+        i2c1.txdr.write(|w| w.txdata().bits(IRA_REG_M));
+
+        // Wait for the byte to send
+        while i2c1.isr.read().tc().bit_is_clear() {}
     }
 
     // Stage 2: Receive the contents of the register we asked for
     let byte = {
-        // TODO Broadcast RESTART
+        // Send START
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Read
+        i2c1.cr2.modify(|r, w| {
+            w.nbytes().bits(1); // read 1 byte
+            w.autoend().set_bit(); // auto-send END once 1 byte is read
+            w.rd_wrn().set_bit(); // READ mode
+            w.start().set_bit() // send START
+        });
 
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Read
+        // Wait until there is data to read
+        while i2c1.isr.read().rxne().bit_is_clear() {}
 
-        // TODO Receive the contents of the register
+        // Receive the contents of the register
+        i2c1.rxdr.read().rxdata().bits()
 
-        // TODO Broadcast STOP
-        0
+        // Broadcast STOP
+        // (we set auto-stop, so no need to send a STOP)
     };
 
     // Expected output: 0x0A - 0b01001000
